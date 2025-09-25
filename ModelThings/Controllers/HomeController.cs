5e22a03490ea72bd4ad70dc7a0bb0e87b51c29e6
GinaXYZ <qiiina122@hotmail.de>
2025-09-25 09:58:27 +0200

    Initial commit for Pythagoras

--- ModelThings/Controllers/HomeController.cs
+++ ModelThings/Controllers/HomeController.cs
@@ -12,45 +12,48 @@ namespace Pythagoras.Controllers
         [HttpPost]
         public IActionResult Index(double? KatheteA, double? KatheteB, double? Hypotenuse)
         {
-
             ViewBag.KatheteA = KatheteA;
             ViewBag.KatheteB = KatheteB;
             ViewBag.Hypotenuse = Hypotenuse;
 
 
             string Fehlermeldung = "";
-            int wasIstZuTun = 0;
-            if (KatheteA is not null) wasIstZuTun += 1;
-            if (KatheteB is not null) wasIstZuTun += 2;
-            if (Hypotenuse is not null) wasIstZuTun += 4;
+            int Calc = 0;
+            if (KatheteA is not null) Calc += 1;
+            if (KatheteB is not null) Calc += 2;
+            if (Hypotenuse is not null) Calc += 4;
 
             if (Hypotenuse < KatheteA || Hypotenuse < KatheteB)
             {
-                wasIstZuTun = 8;
+                Calc = 8;
             }
 
-            switch (wasIstZuTun)
+
+            switch (Calc)
             {
                 case 0:
                     Fehlermeldung = "Bitte mindestens zwei Seitenlängen angeben.";
                     break;
                 case 1:
-                    Fehlermeldung = "Bitte mindestens zwei Seitenlängen angeben.";
+                    Fehlermeldung = "Nur Kathete A wurde angegeben.";
                     break;
                 case 2:
-                    Fehlermeldung = "Bitte mindestens zwei Seitenlängen angeben.";
+                    Fehlermeldung = "Nur Kathete B wurde angegeben.";
                     break;
                 case 3:
                     Hypotenuse = Math.Sqrt((double)((KatheteA * KatheteA) + (KatheteB * KatheteB)));
+                    ViewBag.Hypotenuse = Hypotenuse;
                     break;
                 case 4:
                     Fehlermeldung = "Bitte mindestens zwei Seitenlängen angeben.";
                     break;
                 case 5:
                     KatheteB = Math.Sqrt((double)(Hypotenuse * Hypotenuse - KatheteA * KatheteA));
+                    ViewBag.KatheteB = KatheteB;
                     break;
                     case 6:
                     KatheteA = Math.Sqrt((double)(Hypotenuse * Hypotenuse - KatheteB * KatheteB));
+                    ViewBag.KatheteA = KatheteA;
                     break;
                     case 7:
                     Fehlermeldung = "Es sind bereits alle Seitenlängen angegeben.";
@@ -61,6 +64,7 @@ namespace Pythagoras.Controllers
 
             }
             ViewBag.Fehlermeldung = Fehlermeldung;
+            ViewBag.Calc = Calc;
             return View();
         }
     }
--- ModelThings/obj/Debug/net8.0/Pythagoras.AssemblyInfo.cs
+++ ModelThings/obj/Debug/net8.0/Pythagoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pythagoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c350c5969177330476ecffe02fa515ae553a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pythagoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pythagoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
